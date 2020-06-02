2186dac8b52fcc5464fab1298a9a9c9531bce9f0
Vishnu Reddy <vr@Vishnus-MacBook-Air.local>
2020-06-02 12:56:53 +0530

    Added configuration to app
    Added environment variables and setup configurations for database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from models import db
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config.from_object('config.Config')
+
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+    app.run()
