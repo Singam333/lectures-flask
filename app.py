73eac1c1e4b5888d4a546497033a4dae2d7f8b5a
Vishnu Reddy <vr@Vishnus-MacBook-Air.local>
2020-06-02 15:53:20 +0530

    Added CORS to allow requests from frontend

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+
 from apis import apis
 from models import db
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == '__main__':
     app.config.from_object('config.Config')
